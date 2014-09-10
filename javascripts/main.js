4ec624812f26d40f1c281dd8068bffe45a0879e5
Confectrician <TheViking@ogame.de>
2014-09-10 19:51:32 +0200

    Spoiler for Features. Header Picture and download links only visible at home page

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,6 +1,20 @@
-$(function(){
-  $.when($("#header").load("header.html")).then(function(){
-      $("#main_content").removeClass('invisible');
-      $("#footer").load("footer.html");
-   }); 
+$(function(){    
+    $("#header")
+        .load("header.html", function(){
+            if($('#showmedia').length){
+                $('#media_area')
+                    .load("media_area.html", function(){
+                        $("#main_content")
+                            .removeClass('invisible');
+                        $("#footer")
+                            .load("footer.html");                        
+                    });
+            }
+            else{
+                $("#main_content")
+                    .removeClass('invisible');
+                $("#footer")
+                    .load("footer.html");
+            }
+    });
 });
